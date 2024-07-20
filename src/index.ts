1d6236fa77f7fbe3f793b96ab228dfc1f0af350e
vidit <viditsharma818@gmail.com>
2024-07-20 16:23:35 +0530

    final commit

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://stepsfrontend.vercel.app/",
+    origin: "https://stepsfrontend.vercel.app",
     credentials: true,
   })
 );
